c2354279846d88c7bed0db1030fa54efc8e158b7
Leo Zeng <qingchun1995@gmail.com>
2022-08-14 22:52:50 +0800

    feat: get all products

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,4 @@
+export function getAllProducts() {
+  const products = [1, 2, 3];
+  return products;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
-import type { NextPage } from 'next';
+import { getAllProducts } from 'framework/shopify/product/get-all-products';
+import type { InferGetStaticPropsType } from 'next';
 
-const Home: NextPage = () => {
-  const message: string = 'Hello World';
-  return <div>{message}</div>;
+function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div className="products">{products}</div>;
+}
+
+export const getStaticProps = () => {
+  const products = getAllProducts();
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60 // In seconds
+  };
 };
 
 export default Home;

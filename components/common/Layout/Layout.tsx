e863d4d9d67491696095828b226c749873b30584
Leo Zeng <qingchun1995@gmail.com>
2022-08-18 11:27:56 +0800

    chore: add a comment

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ScriptProps } from 'next/script';
 import React from 'react';
 
+// The easiest way to declare a Function Component; return type is inferred.
 function Layout({ children }: ScriptProps) {
   return <div className="layout">{children}</div>;
 }

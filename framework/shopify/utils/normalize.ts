222a53f54bb31246786b258c4f9dff175263e0e3
Leo Zeng <qingchun1995@gmail.com>
2022-08-18 17:09:29 +0800

    feat: normalize each product's images

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,7 +1,25 @@
-import { Product as ShopifyProduct } from '../schema';
+import { ImageConnection, Product as ShopifyProduct } from '../schema';
+
+export function normalizeProductImages({ edges }: ImageConnection) {
+  return edges.map(({ node: image }) => {
+    const { originalSrc: url, ...rest } = image;
+    return {
+      url: `/images/${url}`,
+      ...rest
+    };
+  });
+}
 
 export function normalizeProduct(productNode: ShopifyProduct) {
-  const { id, title: name, handle, vendor, description, ...rest } = productNode;
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: imageConnection,
+    ...rest
+  } = productNode;
 
   const product = {
     id,
@@ -10,6 +28,7 @@ export function normalizeProduct(productNode: ShopifyProduct) {
     name,
     path: `/${handle}`, // http://localhost:3000/products/cool-hat
     slug: handle.replace(/^\/+|\/+$/g, ''),
+    images: normalizeProductImages(imageConnection),
     ...rest
   };
 

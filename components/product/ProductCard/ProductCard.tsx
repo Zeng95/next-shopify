3ddb11199333e292b00ba111b3bc0a66742b3686
Leo Zeng <qingchun1995@gmail.com>
2022-08-20 16:42:35 +0800

    feat: product image

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { Product } from 'framework/common/types/product';
+import Image from 'next/image';
 import Link from 'next/link';
+import placeholderImage from 'public/product-image-placeholder.svg';
 
 type Props = {
   product: Product;
@@ -7,10 +9,23 @@ type Props = {
 
 function ProductCard({ product }: Props) {
   return (
-    <Link href={`/products/${product.id}`}>
+    <Link href={`/products/${product.slug}`}>
       <div>
-        <h3>{product.name}</h3>
-        <span>$50.00 USD</span>
+        <div>
+          <h3>{product.name}</h3>
+          <span>$50.00 USD</span>
+        </div>
+
+        {product.images && product.images.length && (
+          <Image
+            src={product.images[0].url || placeholderImage}
+            alt={product.name || 'Product Image'}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
       </div>
     </Link>
   );
